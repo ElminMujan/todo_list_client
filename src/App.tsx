aa456f37d4af665c7eca0bccf4489733641d7e97
Elmin Mujan <elminmujan@Elmins-MacBook-Pro.local>
2025-05-20 14:11:25 +0200

    first stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 
+type ItemsType = number[];
+
 function App() {
-  return <></>;
+  const [items, setItems] = useState<ItemsType>([1, 2, 3, 4]);
+
+  const addItem = () => {
+    const n = items.slice(-1)[0] + 1;
+    setItems((prev) => [...prev, n]);
+  };
+
+  return (
+    <div>
+      {items.map((i) => {
+        return <Item item={i} label={"Predmet: "} />;
+      })}
+
+      <button type="button" onClick={addItem}>
+        Novi task
+      </button>
+    </div>
+  );
 }
 
 export default App;
+
+type ItemPropsType = { item: number; label: string };
+
+const Item = (props: ItemPropsType) => {
+  return (
+    <div>
+      <p>
+        {props.label} {props.item}
+      </p>
+    </div>
+  );
+};
