3355227f0d7fcfb263488c5426381fdd4b9e904a
Elmin Mujan <elminmujan@Elmins-MacBook-Pro.local>
2025-05-21 14:19:36 +0200

    Final version app, css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,79 @@
 import { useState } from "react";
 import "./App.css";
 
-type ItemsType = number[];
+type ItemType = {
+  title: string;
+  description: string;
+};
+
+type ItemsType = ItemType[];
 
 function App() {
-  const [items, setItems] = useState<ItemsType>([1, 2, 3, 4]);
+  const [items, setItems] = useState<ItemsType>([]);
+
+  const [taskName, setTaskName] = useState<string>("");
+  const [taskDescription, setTaskDescription] = useState<string>("");
 
   const addItem = () => {
-    const n = items.slice(-1)[0] + 1;
-    setItems((prev) => [...prev, n]);
+    const item: ItemType = { title: taskName, description: taskDescription };
+    if (item.title !== "" && item.description !== "") {
+      setItems((prev) => [...prev, item]);
+      setTaskName("");
+      setTaskDescription("");
+    }
   };
 
+  const clearTaskList = () => {
+    setItems([]);
+  };
   return (
-    <div>
-      {items.map((i) => {
-        return <Item item={i} label={"Predmet: "} />;
-      })}
+    <div className="container">
+      <div className="items">
+        {items.map((i) => {
+          return <Item item={i} />;
+        })}
+      </div>
+      <div className="input-wrapper">
+        <label>Task name</label>
+        <input
+          type="text"
+          value={taskName}
+          onChange={(e) => {
+            setTaskName(e.target.value);
+          }}
+        />
+      </div>
 
-      <button type="button" onClick={addItem}>
-        Novi task
-      </button>
+      <div className="input-wrapper">
+        <label>Description</label>
+        <input
+          type="text"
+          value={taskDescription}
+          onChange={(e) => setTaskDescription(e.target.value)}
+        />
+      </div>
+
+      <div className="actions">
+        <button type="button" onClick={addItem}>
+          Novi task
+        </button>
+        <button type="button" onClick={clearTaskList}>
+          Clear task list
+        </button>
+      </div>
     </div>
   );
 }
 
 export default App;
 
-type ItemPropsType = { item: number; label: string };
+type ItemPropsType = { item: ItemType };
 
 const Item = (props: ItemPropsType) => {
   return (
-    <div>
+    <div className="item-wrapper">
       <p>
-        {props.label} {props.item}
+        {props.item.title} {props.item.description}
       </p>
     </div>
   );
